e6d126daf855b2dce6b5c53a67151f9959d6b710
jirishus <jirishus@yahoo.com>
2025-08-01 15:18:32 -0700

    dockerfile compose index and express

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+// Route
+app.get('/', (req, res) => {
+  res.send('🚀 Node app running inside Docker!');
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});

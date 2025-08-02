dfd04e5f26c4e7870a9641f6e84bd2f7faa68f06
jirishus <jirishus@yahoo.com>
2025-08-02 10:26:37 -0700

    public folder and index file

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
 
+// Serve static files from "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Route
 app.get('/', (req, res) => {
-  res.send('🚀 Node app running inside Docker autodeployed!');
+  // res.send('🚀 Node app running inside Docker autodeployed!');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // HealthCheck
